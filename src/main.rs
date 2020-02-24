bc429cc1989a1a11816b194f0902c1590e1d489f
Justin Lee <justinlee0022@gmail.com>
2020-02-24 11:03:10 -0800

    Added hotloading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use listenfd::ListenFd;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -7,11 +8,14 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(||
         App::new()
             .service(index)
-    })
-    .bind("127.0.0.1:5000")?
-    .run()
-    .await
+    );
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind("127.0.0.1:5000")?,
+    };
+    server.run().await
 }
