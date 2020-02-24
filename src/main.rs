ce798e4cf8dfce80c77896a7452de402c95b24f6
Justin Lee <justinlee0022@gmail.com>
2020-02-24 10:44:55 -0800

    actix setup

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:5000")?
+    .run()
+    .await
+}
