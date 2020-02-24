41873fe99940fa393ad3529bdfb85bcd7f931000
Justin Lee <justinlee0022@gmail.com>
2020-02-24 12:41:14 -0800

    working on models

--- src/user/model.rs
+++ src/user/model.rs
@@ -1,7 +1,57 @@
+use crate::api_error::ApiError;
+use crate::db;
+use crate::schema::user;
+use chrono::{NaiveDateTime, Utc};
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
-pub struct User {
-    pub id: i32,
+#[derive(Serialize, Deserialize, AsChangeset)]
+#[table_name = "user"]
+pub struct UserMessage {
     pub email: String,
+    pub password: String,
 }
+
+#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "user"]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub password: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+impl User {
+  pub fn find_all() -> Result<Vec<Self>, ApiError> {
+    let conn = db::connection()?;
+
+    let users = user::table
+      .load::<User>(&conn)?;
+
+    Ok(users)
+  }
+
+  pub fn find(id: Uuid) -> Result<Self, ApiError> {
+    let conn = db::connection()?;
+
+    let user = user::table
+      .filter(user::id.eq(id))
+      .first(&conn)?;
+
+    Ok(user)
+  }
+
+  pub fn create(user: UserMessage) -> Result<Self, ApiError> {
+    let conn = db::connection()?;
+
+    let user = User::from(user);
+    let user = diesel::insert_into(user::table)
+      .values(user)
+      .get_result(&conn)?;
+
+    Ok(user)
+}
+
+  // pub fn update(id: Uuid)
